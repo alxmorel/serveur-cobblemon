5f4c6559426e2b3a32566518c204cdcaab959a94
alxmorel <alexandre.morel69@gmail.com>
2026-09-19 15:34:52 +0200

    Initialiser le mod Fabric Pokemon World

---
+++ fabric-mod/src/client/java/fr/alxmorel/pokemonworld/client/PokemonWorldClient.java
@@ -0,0 +1,10 @@
+package fr.alxmorel.pokemonworld.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PokemonWorldClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
